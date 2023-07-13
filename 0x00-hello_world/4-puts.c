d0b72d33fd86cb0d1ba8dfc30cd85e1a53f71370
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-13 02:14:26 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: return integer
+ */
+int main(void)
+{
+	puts("\"Progamming is like building a multilingual puzzle");
+
+	return (0);
+}
