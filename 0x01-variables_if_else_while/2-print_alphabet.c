8d445cd117bd91886520657c4c7bab400af7d979
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-14 06:29:13 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: zero on success
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		printf("%c", i);
+	}
+
+	return (0);
+}
