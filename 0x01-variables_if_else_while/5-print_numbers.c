8f52953dd9e382418b758540dd83fce1bb43c86c
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-14 07:25:17 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include ,stdio.h>
+
+/**
+ * main - main function
+ * Return: returns 0 on succes
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include ,stdio.h>
+
+/**
+ * main - main function
+ * Return: returns 0 on succes
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
