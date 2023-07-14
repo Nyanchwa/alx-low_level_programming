72be700559d72c05b3e4760cbbe237592dac1a03
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-14 06:41:09 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: zero on success
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+		putchar(i);
+		i++;
+	}
+
+	putchar("\n");
+	return (0);
+}
