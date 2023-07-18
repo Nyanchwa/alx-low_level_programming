33ae90819a35e1af0d3026fdc6481761aa34b8f0
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-18 11:24:17 -0700

    sum of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: zero on success
+ */
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+		i++;
+	}
+	return (0);
+}
