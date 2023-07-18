af857ccbdc49c97c3fd8143babd3792c8db466a3
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-18 03:51:16 -0700

    prints alphabets x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,16 @@
 void print_alphabet_x10(void)
 {
 	int i = 97;
-	int j;
+	int j = 0;
 
-	for (j = 0; j < 10; j++)
+	while (j < 10)
 	{
 		while (i <= 122)
 		{
 			_putchar(i);
 			i++;
 		}
+		j++;
 	}
 	_putchar('\n');
 }
