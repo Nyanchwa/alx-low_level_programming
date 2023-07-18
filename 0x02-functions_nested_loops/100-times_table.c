5cf63369fd25b2b8c142005f3a3c151275b4679a
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-18 05:46:31 -0700

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - 9 times table
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(48);
+		for (j = 1; j < n; j++)
+		{
+			k = j * i;
+			if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(48 + k / 10);
+				_putchar(48 + k % 10);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(48 + k);
+			}
+		}
+		_putchar('\n');
+	}
+}
