d5140688e81707095d5b5e5ab0d7f68e6dceafcb
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-18 05:37:21 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,46 +9,17 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			if (n < 10)
-			{
-				if (n < 0)
-				{
-					n = -n;
-				}
-				_putchar(48 + n);
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(48 + n / 10);
-				_putchar(48 + n % 10);
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", n);
 			n++;
 		}
 	}
-	else if (n == 99)
-	{
-		_putchar(48 + n / 10);
-		_putchar(48 + n % 10);
-		_putchar(',');
-		_putchar(' ');
-	}
-	else if (n >= 100)
+	else if (n > 98)
 	{
 		while (n > 98)
 		{
-			_putchar(48 + n / 100);
-			_putchar(48 + n % 100);
-			_putchar(48 + n % 10);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 			n--;
 		}
 	}
-	_putchar(57);
-	_putchar(56);
-	_putchar('\n');
+	printf("%d\n", 98);
 }
