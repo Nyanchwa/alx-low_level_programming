75ab8c52bbb662094cb5ae0b212cf0ae7dfd494c
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-24 10:40:11 -0700

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ * Return: rturn value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	while (i <= len)
+	{
+		if (i == len)
+		{
+			dest[i] = '\0';
+		}
+		dest[i] = src[i];
+		i++;
+	}
+	_putchar('\n');
+}
