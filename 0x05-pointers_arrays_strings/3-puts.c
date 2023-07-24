d39e54362350b9c41a02cdd34c0bc3c19f77ef7e
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-24 05:44:04 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - writes a string to STDOUT
+ * @str: adress
+ */
+void _puts(char *str)
+{
+	char c;
+
+	while (*str != '\0')
+	{
+		c = *str;
+
+		_putchar(c);
+		str++;
+	}
+}
