36ce4398ca6b2a1c0194e1e8aafdaed1620a560e
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-26 02:28:34 -0700

    concatinate n charcters

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
+	int k;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	k = i + n;
 
-	while (i < (i + n))
+	while (src[j] != '\0' && (i < k))
 	{
 		dest[i] = src[j];
 		i++;
