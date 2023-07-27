0a2937950c1a74a4defb25158e70e06db6c30d30
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-27 15:47:00 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize
+ * @s: string
+ * Return: pointer
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		{
+			i++;
+		}
+		if (s[i - 1] == ' ' ||
+			s[i - 1] == ',' ||
+			s[i - 1] == '.' ||
+			s[i - 1] == ';' ||
+			s[i - 1] == '!' ||
+			s[i - 1] == '?' ||
+			s[i - 1] == '"' ||
+			s[i - 1] == '(' ||
+			s[i - 1] == ')' ||
+			s[i - 1] == '{' ||
+			s[i - 1] == '}' ||
+			i == 0)
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
