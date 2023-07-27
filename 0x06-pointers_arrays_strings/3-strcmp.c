e8c37b2dad658354b840e7d3db07ef8d77840207
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-27 14:59:30 -0700

    compare a string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - string comparison
+ * @s1: string 1
+ * @s2: string 2
+ * Return: an int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] < s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
