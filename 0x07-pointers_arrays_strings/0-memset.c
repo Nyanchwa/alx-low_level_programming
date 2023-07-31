136af44c1f9eced5406ac43bfab6c5df2482505e
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-31 03:54:31 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	if (*b == NULL)
-	{
-		return (s);
-	}
 	while (i < n)
 	{
-		s[i] = c;
+		s[i] = b;
 		i++;
 	}
 	return (s);
