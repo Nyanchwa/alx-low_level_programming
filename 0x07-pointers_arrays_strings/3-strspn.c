a4041ff9922af6470bac53fad8ac0630e19abd23
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-31 05:27:25 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,15 +8,16 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	unsigned int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[j])
 			{
+				i++;
 				break;
 			}
 			else if (accept[j + 1] == '\0')
@@ -24,6 +25,7 @@ unsigned int _strspn(char *s, char *accept)
 				return (i);
 			}
 		}
+		s++;
 	}
 	return (i);
 }
