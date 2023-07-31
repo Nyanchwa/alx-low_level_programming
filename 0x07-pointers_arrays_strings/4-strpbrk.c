391353cd668df8d91aa98532ea807cebf3b23a4c
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-31 05:19:08 -0700

    strpbk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function
+ * @s: string
+ * @accept: string
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (s);
+}
