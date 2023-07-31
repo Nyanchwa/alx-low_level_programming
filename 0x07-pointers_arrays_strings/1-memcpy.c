5470de156e576cb35c9274fdd814bcb63d0ae20f
Nyanchwa <leeroynyanchwa@gmail.com>
2023-07-31 04:16:56 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpyn - copies a string to another
+ * _memcpy - copies a string to another
  * @dest: destination
  * @src: source
  * @n: int
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while(i < n)
+	while (i < n)
 	{
 		dest[i] = src[i];
 		i++;
