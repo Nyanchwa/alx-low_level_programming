71fcfeea6c7258bf5082de3d1d164878ab7a5d28
Nyanchwa <leeroynyanchwa@gmail.com>
2023-08-02 13:43:52 -0700

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,13 @@
 /**
  * factorial - returns the factorial of a number
  * @n: number to return the factorial from
- *
  * Return: factorial of n
  */
 int factorial(int n)
 {
 	if (n < 0)
 		returrn (-1);
-	if (n == 0)
+	else if (n == 0)
 		return (1);
 
 	return (n * factorial(n - 1));
