e536fae8b7c467277478394fa63c537be4d10cf5
Nyanchwa <leeroynyanchwa@gmail.com>
2023-08-08 11:14:39 -0700

    concatinate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,13 @@ char *str_concat(char *s1, char *s2)
 	int k = 0;
 	int l = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	if (s1 != NULL)
