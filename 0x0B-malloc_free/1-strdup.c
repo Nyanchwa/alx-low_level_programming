5c3eb2451ccda5c12bc0e24ed03738c1f54db0f3
Nyanchwa <leeroynyanchwa@gmail.com>
2023-08-08 00:58:19 -0700

    duplicate a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: string
+ * Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	char *buffer;
+	int i = 0;
+	int len;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	if (str != NULL)
+	{
+		for (len = 0; str[len];)
+		{
+			len++;
+		}
+	}
+	buffer = malloc(len * sizeof(char));
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len)
+	{
+		buffer[i] = str[i];
+		i++;
+	}
+	return (buffer);
+}
