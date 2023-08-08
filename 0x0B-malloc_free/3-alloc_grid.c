c18f3f40b8d6fedab6b2a593284cbc2db63f9188
Nyanchwa <leeroynyanchwa@gmail.com>
2023-08-08 12:04:34 -0700

    allocate grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * alloc_grid - creates 2D array
+ * @width: columns
+ * @height: rows
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int **buffer = malloc(height * sizeof(int *));
+	int i = 0;
+	int j = 0;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		buffer[i] = malloc(width * sizeof(int));
+		for (j = 0; j < width; j++)
+		{
+			buffer[i][j] = 0;
+		}
+	}
+	return (buffer);
+}
